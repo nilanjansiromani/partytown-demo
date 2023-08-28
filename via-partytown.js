9c9730b475724dd0089788d743a6b2a9d0db887f
n0s00jx <nilajan.siromani@walmart.com>
2023-08-29 00:11:00 +0530

    working example

--- via-partytown.js
+++ via-partytown.js
@@ -1,1 +1,19 @@
 console.log("Loaded via partywown");
+/**
+ * This operation happens only on the worker and does not percolate to the main thread
+ */
+performance.mark("longop-start");
+for (let i = 0; i < 100000; i++) {
+  Math.sinh(Math.acosh(Math.tanh(Math.sqrt(i))));
+}
+performance.mark("longop-end");
+performance.measure("long-op", "longop-start", "longop-end");
+
+/**
+ * This operation happens on the main thread.
+ */
+const id = document.getElementById("viaPartytown");
+const node = document.createTextNode(
+  `This was created at ${Date()} USING PARTYTOWN`
+);
+id.appendChild(node);
